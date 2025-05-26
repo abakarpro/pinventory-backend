82bae285b01725d439f4dc9d0be554894f6c806b
abakarpro <abakarpro@gmail.com>
2025-05-26 03:59:03 +0200

    update server.js to fix issue #123

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use("/api/products", productRoute);
 app.use("/api/contactus", contactRoute);
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 //Routes
 app.get("/", (req, res) => {
